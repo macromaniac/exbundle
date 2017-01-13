05ae67e483ee2528a757deeb230261b7262b7b87
macro <joeycaero@gmail.com>
2017-01-13 13:07:15 -0600

    ex

---
+++ test/hello.ts
@@ -0,0 +1,5 @@
+﻿export class Hello {
+	constructor() {
+		console.log("Hello !");
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+﻿var path = require("path");
+
+module.exports = {
+	entry: { 'main': './js/app.js' },
+	debug: true,
+	devtool: 'source-map',
+	output: {
+		filename: '[name].bundle.js',
+		devtoolModuleFilenameTemplate: '[resource-path]'
+	},
+	module: {
+		preLoaders: [
+		  {
+		  	test: /\.js$/,
+		  	loader: "source-map-loader"
+		  }
+		]
+	}
+};
